6466340c83821c60651e9d8612baea6d0bc3358e
mufaqar <mufaqar@gmail.com>
2024-01-18 20:41:10 +0500

    Logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/header'
 import Footer from './components/footer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Billr Ltd | You can ear points on all your business transactions',
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         {children}
         <Footer />
